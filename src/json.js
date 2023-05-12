832c2b0bcc48f11e940938ca258337b86e7cdec6
mayssam73 <mayssamkalajo@gmail.com>
2023-05-12 06:43:42 -0500

    Almost done!

--- src/json.js
+++ src/json.js
@@ -1,10 +1,20 @@
 const data = [{
     num: "COSC 1336",
-    name: "Computer Science & Programming",
+    name: "Computer Science and Programming",
     creditHours: 3,
     prereq: "A grade of C- or better in MATH 1314 or equivalent.",
     description: "Introduction to problem solving through computer programming. Students will learn the fundamental principles of computer science, basic hardware and software components of a computer system, computational thinking, basic algorithms, and programming concepts. Students will get hands-on experience in problem solving by designing, writing, testing and debugging programs in a modern programming language.",
-    repeatability: "No"
+    repeatability: "No",
+    classType: "None"
+},
+{
+    num: "COSC 1437",
+    name: "Introduction to Programming",
+    creditHours: 4,
+    prereq: "A grade of C- or better in COSC 1336, and credit for or concurrent enrollment in MATH 2413.",
+    description: "Fundamental concepts of structured programming; procedures and elementary data structures with a focus on problem solving strategies and implementation; computer organization, structured procedural programming, C/C++ programming language, and algorithm design.",
+    repeatability: "No",
+    classType: "None"
 },
 {
     num: "ENGL 1301",
@@ -12,6 +22,212 @@ const data = [{
     creditHours: 3,
     prereq: "A TSI placement score of at least 340, and an essay score of at least a 4 on the TSI Writing Assessment; or a placement score of less than 340, and an ABE Diagnostic level of at least 4, and an essay score of at least 5 on the TSI Writing Assessment; and a score ranging from 351 to 390 on the TSI Reading Assessment. Students may exempt the TSI by obtaining satisfactory scores on other accepted standardized tests. See http://www.uh.edu/ussc/tsi/ for details.",
     description: "None",
-    repeatability: "No"
+    repeatability: "No",
+    classType: "None"
 },
+{
+    num: "ENGL 1302",
+    name: "First Year Writing II",
+    creditHours: 3,
+    prereq: "ENGL 1301 or equivalent.",
+    description: "Detailed study of the principles of rhetoric as applied to analyzing and writing argumentative and persuasive essays; principles and methods of research, culminating in writing a substantial research paper.",
+    repeatability: "No",
+    classType: "None"
+},
+{
+    num: "HIST 1301",
+    name: "The U.S. to 1877",
+    creditHours: 3,
+    prereq: "None",
+    description: "The social, economic, and political history of the United States to 1877.",
+    repeatability: "No",
+    classType: "None"
+},
+{
+    num: "HIST 1302",
+    name: "The U.S. Since 1877",
+    creditHours: 3,
+    prereq: "None",
+    description: "The social, economic, and political history of the United States since 1877.",
+    repeatability: "No",
+    classType: "None"
+},
+{
+    num: "GOVT 2306",
+    name: "US and Texas Constitution and Politics",
+    creditHours: 3,
+    prereq: "None",
+    description: "Introduction to the constitutions and politics of the United States and Texas, emphasizing constitutional structure, federalism, separation of powers, limited government, public opinion, elections, and civil liberties. (Introductory category).",
+    repeatability: "No",
+    classType: "None"
+},
+{
+    num: "GOVT 2305",
+    name: "US Government: Congress, President, & Courts",
+    creditHours: 3,
+    prereq: "None",
+    description: "Introduction to the institutions of government with an emphasis on the congress, the president, and the federal courts. (Introductory category).",
+    repeatability: "No",
+    classType: "None"
+},
+{
+    num: "MATH 2413",
+    name: "Calculus I",
+    creditHours: 4,
+    prereq: "Credit for MATH 2312 with a grade of C- or higher, or a satisfactory score on a placement examination.",
+    description: "Calculus of rational functions, limits, derivatives, applications of the derivative, antiderivatives, the definite integral with applications, mean value theorem, fundamental theorem of calculus, and numerical integration.",
+    repeatability: "Yes",
+    classType: "None"
+},
+{
+    num: "MATH 2414",
+    name: "Calculus II",
+    creditHours: 4,
+    prereq: "Credit for MATH 2413 with a grade of C- or higher.",
+    description: "Calculus of transcendental functions: additional techniques and applications of integration, indeterminate forms, improper integrals, Taylor’s formula, and infinite series.",
+    repeatability: "Yes",
+    classType: "None"
+},
+{
+    num: "COSC 2436",
+    name: "Programming and Data Structures",
+    creditHours: 4,
+    prereq: "A grade of C- or better in COSC 1437 and credit for or concurrent enrollment in MATH 2414.",
+    description: "Introduction to fundamental data structures: arrays, lists, stacks, queues, hash tables, trees; sorting and searching; graph algorithms; design, analysis, and comparison of algorithms. Correctness verification techniques such as assertions and invariants. Review program specification, unit testing, and debugging.",
+    repeatability: "No",
+    classType: "None"
+},
+{
+    num: "CORE",
+    name: "Creative Arts",
+    creditHours: 3,
+    prereq: "None",
+    description: "None",
+    repeatability: "No",
+    classType: "Creative Arts"
+},
+{
+    num: "COSC 2425",
+    name: "Computer Organization and Architecture",
+    creditHours: 4,
+    prereq: "A grade of C- or better in COSC 1437.",
+    description: "Low-level computer design, basics of digital design, and hardware/software interface; Principles of pipelining and caching, instruction pipelining, SIMD and multiprocessor systems.",
+    repeatability: "No",
+    classType: "None"
+},
+{
+    num: "TEST",
+    name: "First Year Writing II",
+    creditHours: 3,
+    prereq: "ENGL 1301 or equivalent.",
+    description: "Detailed study of the principles of rhetoric as applied to analyzing and writing argumentative and persuasive essays; principles and methods of research, culminating in writing a substantial research paper.",
+    repeatability: "No",
+    classType: "None"
+},
+{
+    num: "TEST",
+    name: "The U.S. to 1877",
+    creditHours: 3,
+    prereq: "None",
+    description: "The social, economic, and political history of the United States to 1877.",
+    repeatability: "No",
+    classType: "None"
+},
+{
+    num: "TEST",
+    name: "The U.S. Since 1877",
+    creditHours: 3,
+    prereq: "None",
+    description: "The social, economic, and political history of the United States since 1877.",
+    repeatability: "No",
+    classType: "None"
+},
+{
+    num: "TEST",
+    name: "US and Texas Constitution and Politics",
+    creditHours: 3,
+    prereq: "None",
+    description: "Introduction to the constitutions and politics of the United States and Texas, emphasizing constitutional structure, federalism, separation of powers, limited government, public opinion, elections, and civil liberties. (Introductory category).",
+    repeatability: "No",
+    classType: "None"
+},
+{
+    num: "TEST",
+    name: "US Government: Congress, President, & Courts",
+    creditHours: 3,
+    prereq: "None",
+    description: "Introduction to the institutions of government with an emphasis on the congress, the president, and the federal courts. (Introductory category).",
+    repeatability: "No",
+    classType: "None"
+},
+{
+    num: "TEST",
+    name: "Calculus I",
+    creditHours: 4,
+    prereq: "Credit for MATH 2312 with a grade of C- or higher, or a satisfactory score on a placement examination.",
+    description: "Calculus of rational functions, limits, derivatives, applications of the derivative, antiderivatives, the definite integral with applications, mean value theorem, fundamental theorem of calculus, and numerical integration.",
+    repeatability: "Yes",
+    classType: "None"
+},
+{
+    num: "TEST",
+    name: "Calculus II",
+    creditHours: 4,
+    prereq: "Credit for MATH 2413 with a grade of C- or higher.",
+    description: "Calculus of transcendental functions: additional techniques and applications of integration, indeterminate forms, improper integrals, Taylor’s formula, and infinite series.",
+    repeatability: "Yes",
+    classType: "None"
+},
+{
+    num: "TEST",
+    name: "Calculus II",
+    creditHours: 4,
+    prereq: "Credit for MATH 2413 with a grade of C- or higher.",
+    description: "Calculus of transcendental functions: additional techniques and applications of integration, indeterminate forms, improper integrals, Taylor’s formula, and infinite series.",
+    repeatability: "Yes",
+    classType: "None"
+},
+]
+
+const creativeArts = [
+    {
+        num: "ARTS 1304",
+        name: "Art History II (15th Century to the Present",
+        creditHours: 3,
+        prereq: "ENGL 1301.",
+        description: "Art and its relationship to society in and for which it was made from the Renaissance to the present.",
+        repeatability: "No"
+    },
+    {
+        num: "DANC 2303",
+        name: "Introduction to Dance",
+        creditHours: 3,
+        prereq: "ENGL 1302.",
+        description: "An introductory class to dance as an art form with an overview of the dance techniques, history, and choreography of major dance genres.",
+        repeatability: "Yes"
+    },
+    {
+        num: "DRAM 1310",
+        name: "Introduction to Theatre",
+        creditHours: 3,
+        prereq: "None",
+        description: "Students will gain a genuine appreciation for the art and craft of theatre by reading, attending, and writing critically about plays.",
+        repeatability: "No"
+    },
+    {
+        num: "INDS 2355",
+        name: "Design History I",
+        creditHours: 3,
+        prereq: "Credit for or concurrent enrollment in ENGL 1302.",
+        description: "Survey of the history of design and art from the age of the industrial revolution to the postwar period with emphasis on the influence of cultural, philosophical, and technical forces.",
+        repeatability: "No"
+    },
+    {
+        num: "PHIL 1361",
+        name: "Philosophy and the Arts",
+        creditHours: 3,
+        prereq: "ENGL 1301.",
+        description: "Introduction to philosophical topics in the visual and performing arts, including criticism, interpretation, moral issues, and cultural contexts.",
+        repeatability: "No"
+    },
 ]
--- src/script.js
+++ src/script.js
@@ -2,25 +2,30 @@ window.onload = function() {
     localStorage.setItem("data", JSON.stringify(data));
     let parsedData = JSON.parse(localStorage.getItem("data"));
 
-    function showTable(parsedData) {
+    localStorage.setItem("creativeArts", JSON.stringify(creativeArts));
+    let parsedCreativeArts = JSON.parse(localStorage.getItem("creativeArts"));
+
+    function showTable(parsedData, tableNum, courseStart, courseNum) {
         var totalHoursFall = 0;
         var totalHoursSpring = 0;
-
+        
         let table =
             `<tr>
-                <th class="year" rowspan="7">Year 1</th>
+                <th class="year" rowspan="7">Year ${tableNum}</th>
                 <th class="header" colspan="3">Semester 1 Fall</th>
                 <th class="header" colspan="3">Semester 2 Spring</th>
+                <th class="header">Total</th>
             </tr>`;
 
-        for (i = 1; i < parsedData.length; i++) {
+        for (i = courseStart; i < courseNum; i+=2) {
             table += `<tr>
-                <td class="clickable" id="${i-1}">${parsedData[i - 1].num}</td>
-                <td class="clickable" id="${i-1}">${parsedData[i - 1].name}</td>
-                <td class="clickable" id="${i-1}">${parsedData[i - 1].creditHours}</td>
-                <td class="clickable" id="${i}">${parsedData[i].num}</td>
-                <td class="clickable" id="${i}">${parsedData[i].name}</td>
-                <td class="clickable" id="${i}">${parsedData[i].creditHours}</td>
+                <td class="clickable" id="i${i-1}">${parsedData[i - 1].num}</td>
+                <td class="clickable" id="i${i-1}">${parsedData[i - 1].name}</td>
+                <td class="clickable" id="i${i-1}">${parsedData[i - 1].creditHours}</td>
+                <td class="clickable" id="i${i}">${parsedData[i].num}</td>
+                <td class="clickable" id="i${i}">${parsedData[i].name}</td>
+                <td class="clickable" id="i${i}">${parsedData[i].creditHours}</td>
+                <td></td>
             </tr>`;
             
             totalHoursFall += parsedData[i - 1].creditHours;
@@ -28,35 +33,138 @@ window.onload = function() {
         }
         
         table += 
-            `<td colspan="2">Semester Hours</td>
-            <td>${totalHoursFall}</td>
-            <td colspan="2">Semester Hours</td>
-            <td>${totalHoursSpring}</td>`
+            `<tr class="hours">
+                <td colspan="2">Semester Hours</td>
+                <td>${totalHoursFall}</td>
+                <td colspan="2">Semester Hours</td>
+                <td>${totalHoursSpring}</td>
+                <td style="text-align: center">${totalHoursFall + totalHoursSpring}</td>
+            </tr>`
 
-        document.getElementById("table").innerHTML = table;
-    }
+        document.getElementById("table" + tableNum).innerHTML = table;
 
-    showTable(parsedData);
-
-    var classes = document.getElementsByClassName("clickable");
-    for (var i = 0; i < classes.length; i++) {
-        classes[i].addEventListener("click", showDescription);
+        var classes = document.getElementsByClassName("clickable");
+        for (var i = 0; i < classes.length; i++) {
+            classes[i].addEventListener("click", showDescription);
+        }
     }
     
     function showDescription() {
+        var id = this.id.slice(1);
+
         let description = 
-            `<h3>${parsedData[this.id].num} - ${parsedData[this.id].name}</h3>
-            <h4>Credit Hours: ${parsedData[this.id].creditHours}</h4>
+            `<h3>${parsedData[id].num} - ${parsedData[id].name}</h3>
+            <h4>Credit Hours: ${parsedData[id].creditHours}</h4>
             <hr></hr>
             <h4>Prerequisite:</h4>
-            <p> ${parsedData[this.id].prereq}</p><br />
+            <p> ${parsedData[id].prereq}</p><br />
             <h4>Description:</h4>
-            <p> ${parsedData[this.id].description}</p><br />
+            <p> ${parsedData[id].description}</p><br />
             <h4>Repeatability:</h4>
-            <p> ${parsedData[this.id].repeatability}</p>
+            <p> ${parsedData[id].repeatability}</p>
             `
 
-        document.getElementById("description").innerHTML = description;
-        document.getElementById("description").classList.toggle("show");
+        var currDescription;
+        if (this.id <= 10) {
+            currDescription = document.getElementById("description1");
+        }
+        else if (this.id <= 21) {
+            currDescription = document.getElementById("description2");
+        }
+        else if (this.id <= 32) {
+            currDescription = document.getElementById("description3");
+        }
+        else {
+            currDescription = document.getElementById("description4");
+        }
+
+        currDescription.innerHTML = description;
+
+        // var prevOpenDescription = document.getElementsByClassName("show")[0];
+        // prevOpenDescription.classList.remove("show");
+        currDescription.classList.add("show");
     }
+
+    function showDropdownDescription(object, myData) {
+        var value = object.value;
+
+        if (value !== "default") {
+            let description = 
+                `<h3>${myData[value].num} - ${myData[value].name}</h3>
+                <h4>Credit Hours: ${myData[value].creditHours}</h4>
+                <hr></hr>
+                <h4>Prerequisite:</h4>
+                <p> ${myData[value].prereq}</p><br />
+                <h4>Description:</h4>
+                <p> ${myData[value].description}</p><br />
+                <h4>Repeatability:</h4>
+                <p> ${myData[value].repeatability}</p>
+                `
+
+            var currDescription;
+            var parent = object.parentNode.parentNode;
+            var parentId = parent.id.slice(1);
+
+            if (parentId <= 10) {
+                currDescription = document.getElementById("description1");
+            }
+            else if (parentId <= 21) {
+                currDescription = document.getElementById("description2");
+            }
+            else if (parentId <= 32) {
+                currDescription = document.getElementById("description3");
+            }
+            else {
+                currDescription = document.getElementById("description4");
+            }
+
+            currDescription.innerHTML = description;
+
+            // var prevOpenDescription = document.getElementsByClassName("show")[0];
+            // prevOpenDescription.classList.remove("show");
+            currDescription.classList.add("show");
+        }
+    }
+
+    function addCourseSelectors(courseStart, courseNum) {
+        var creativeArtsDropdown;
+
+        for (i = courseStart - 1; i < courseNum; i++) {
+            if (parsedData[i].classType !== "None") {
+                document.querySelectorAll('#i' + i)[0].removeEventListener("click", showDescription);
+                document.querySelectorAll('#i' + i)[1].removeEventListener("click", showDescription);
+                document.querySelectorAll('#i' + i)[2].removeEventListener("click", showDescription);
+            }
+            if (parsedData[i].classType === "Creative Arts") {
+                creativeArtsDropdown = 
+                    `<select class="creativeArts">
+                        <option value="default" selected>Select Course</option>`;
+
+                for (y = 0; y < parsedCreativeArts.length; y++) {
+                    var myOption = `<option value=${y}>${parsedCreativeArts[y].num}</option>`;
+                    creativeArtsDropdown += myOption;
+                }
+
+                creativeArtsDropdown += `</select>`
+
+                document.querySelectorAll('#i' + i)[0].innerHTML += creativeArtsDropdown;
+            }
+        }
+
+        var creativeArtsDropdown = document.getElementsByClassName("creativeArts");
+        for (var i = 0; i < creativeArtsDropdown.length; i++) {
+            creativeArtsDropdown[i].addEventListener("click", function() {
+                showDropdownDescription(this.options[this.selectedIndex], parsedCreativeArts)
+            });
+        }
+    }
+
+    showTable(parsedData, 1, 1, 10);
+    addCourseSelectors(1, 10);
+
+    showTable(parsedData, 2, 11, 21);
+    addCourseSelectors(11, 21);
+    // showTable(parsedData, 3, 23, 10);
+    // showTable(parsedData, 4, 34, 10);
+
 }
